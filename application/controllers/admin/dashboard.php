c4060a326b5456c5342e162229ea60fbaee2d555
unknown <liu@.(none)>
2013-08-25 22:25:51 +0800

    add boostrap support

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -1,7 +1,15 @@
 <?php
-class Dashboard extends MY_Controller {
+class Dashboard extends Admin_Controller {
 	
 	function __construct($argument) {
 		parent :: __construct();
 	}
+    
+    public function index() {
+        $this->view = 'admin/_layout_main';
+    }
+    
+    public function modal() {
+        $this->view = 'admin/_layout_modal';
+    }
 }
---
+++ application/views/admin/_layout_main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://lib.sinaapp.com/js/jquery/1.6/jquery.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
