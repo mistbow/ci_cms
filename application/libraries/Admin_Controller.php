3ebaebf34756edad5b167b69f1a94428f75ff780
unknown <liu@.(none)>
2013-08-25 23:26:51 +0800

    move url helper from fontend_controller to admin controller

--- application/libraries/Admin_Controller.php
+++ application/libraries/Admin_Controller.php
@@ -1,6 +1,8 @@
 <?php
 class Admin_Controller extends MY_Controller {
 
+    protected $helpers = array( 'url' );
+
     function __construct() {
         parent::__construct();
         $this->data['meta_title'] = '6bey website CMS';
--- application/libraries/Frontend_Controller.php
+++ application/libraries/Frontend_Controller.php
@@ -2,8 +2,6 @@
 class Frontend_Controller extends MY_Controller
 {
 
-    protected $helpers = array( 'url' );
-
 	function __construct ()
 	{
 		parent::__construct();
