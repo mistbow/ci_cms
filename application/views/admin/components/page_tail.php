01e2798cecd2438276aef80b64876b2daff5ae01
unknown <liu@.(none)>
2013-08-25 23:06:34 +0800

    add header and tail files

---
+++ application/views/admin/components/page_tail.php
@@ -0,0 +1,7 @@
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://lib.sinaapp.com/js/jquery/1.6/jquery.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- application/views/layouts/dashboard.php
+++ application/views/layouts/dashboard.php
@@ -1,26 +1,60 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title><?php echo $meta_title; ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
+<?php $this->load->view('admin/components/page_head'); ?>
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo site_url('admin/dashboard'); ?>">6bey</a>
+          </div>
+        
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li><?php echo anchor('admin/page', 'pages'); ?></li>
+                <li><?php echo anchor('admin/page/order', 'order pages'); ?></li>
+                <li><?php echo anchor('admin/article', 'news articles'); ?></li>
+                <li><?php echo anchor('admin/user', 'users'); ?></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </nav>
       
+      <div class="container">
+        <div class="row">
+            <!-- Main column -->
+            <div class="span9">
+                <h2>page</h2>
+            </div>
+            <!-- Sidebar -->
+            <div class="span3">
+                <section>
+                    <?php echo mailto('117064092@qq.com', '<i class="icon-user"></i> 117064092@qq.com'); ?><br>
+                    <?php echo anchor('admin/user/logout', '<i class="icon-off"></i> logout'); ?>
+                </section>
+            </div>
+        </div>
+    </div>
+    
     <?php echo $yield; ?>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="http://lib.sinaapp.com/js/jquery/1.6/jquery.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php $this->load->view('admin/components/page_tail'); ?>
