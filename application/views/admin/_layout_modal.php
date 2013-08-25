94aad18454276cc9c73e5d42bfb68d814ae869b3
unknown <liu@.(none)>
2013-08-25 23:46:32 +0800

    add login code

--- application/views/admin/_layout_modal.php
+++ application/views/admin/_layout_modal.php
@@ -2,14 +2,22 @@
 
 <body style="background: #555;">
 
-    <div class="modal show" role="dialog">
-        <div class="modal-header">
-            <h3>Log in</h3>
-        </div>
-        <div class="modal-body"></div>
-        <div class="modal-footer">
+    <div class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Log in</h4>
+          </div>
+          <div class="modal-body">
+            <p>One fine body&hellip;</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
             &copy; <?php echo date('Y'); ?> <?php echo $meta_title; ?>
-        </div>
-    </div>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
 <?php $this->load->view('admin/components/page_tail'); ?>
