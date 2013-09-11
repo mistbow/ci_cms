de82f3e8eedce6b0b6b50eba8d152478a01322ef
unknown <liu@.(none)>
2013-09-11 21:58:01 +0800

    add login page

---
+++ application/views/components/page_tail.php
@@ -0,0 +1,5 @@
+<<script src="http://code.jquery.com/jquery-latest.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,1 +1,21 @@
-login page
+<?php $this->load->view('components/page_head'); ?>
+<body style="background: #555;">
+
+    <div class="modal show" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Log in</h4>
+          </div>
+          <div class="modal-body">
+            <p>One fine body&hellip;</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+            &copy; <?php echo date('Y'); ?> <?php echo $meta_title; ?>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+<?php $this->load->view('components/page_head'); ?>
