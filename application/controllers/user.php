05ac30dd881e55bc9109946dc307bbca8d45785f
unknown <liu@.(none)>
2013-09-13 22:27:20 +0800

    call user login function

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -13,10 +13,18 @@ class User extends Frontend_Controller {
 	}
     
     public function login() {
+    	$dashboard = 'admin/dashboard';
     	$validate = $this->user->login_validate;
 		$this->form_validation->set_rules($validate);
     	if ($this->form_validation->run() == TRUE) {
     		// We can login and redirect
+    		if ($this->user->login() == TRUE) {
+    			redirect($dashboard);
+    		}
+    		else {
+    			$this->session->set_flashdata('error', '账号或者密码错误');
+    			redirect('user/login', 'refresh');
+    		}
     	}
     	$this->data['subview'] = 'components/login_subview';
     }
