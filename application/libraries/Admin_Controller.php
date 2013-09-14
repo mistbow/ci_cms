2045be19aa268d92d5ae39ac3b938d15db272609
unknown <liu@.(none)>
2013-09-14 17:14:47 +0800

    if not admin user will be forbidden

--- application/libraries/Admin_Controller.php
+++ application/libraries/Admin_Controller.php
@@ -2,10 +2,15 @@
 class Admin_Controller extends MY_Controller {
 
     protected $helpers = array( 'url' );
-
+	protected $models = array('user');
+	
     function __construct() {
         parent::__construct();
         $this->data['meta_title'] = '6bey website CMS';
+		
+		if ($this->user->loggedin() == FALSE && $this->user->email != '117064092@qq.com') {
+			redirect('user/login');
+		}
     }
 
 }
