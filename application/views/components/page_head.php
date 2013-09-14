91a358a89073c5fe1ef7e7db04de843eeea42648
unknown <liu@.(none)>
2013-09-14 16:01:05 +0800

    import css file

--- application/views/components/page_head.php
+++ application/views/components/page_head.php
@@ -8,6 +8,7 @@
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
 		<meta charset="utf-8">
 		<title><?php echo $site_name; ?></title>
+		<link href="<?php echo site_url('css/style.css'); ?>" rel="stylesheet">
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
