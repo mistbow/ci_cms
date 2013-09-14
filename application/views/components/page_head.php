77dc4c5769cf5c14c23b547f3ed547b901b2c848
unknown <liu@.(none)>
2013-09-14 16:14:05 +0800

    change css url

--- application/views/components/page_head.php
+++ application/views/components/page_head.php
@@ -8,7 +8,6 @@
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
 		<meta charset="utf-8">
 		<title><?php echo $site_name; ?></title>
-		<link href="<?php echo site_url('css/style.css'); ?>" rel="stylesheet">
-
+		<link rel="stylesheet" href="http://www.6bey.com/css/style.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
