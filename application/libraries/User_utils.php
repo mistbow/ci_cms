02aaf24e715c3f4c55aac712b0a0beb835c4534e
unknown <liu@.(none)>
2013-09-28 21:48:47 +0800

    load session library

--- application/libraries/User_utils.php
+++ application/libraries/User_utils.php
@@ -1,11 +1,22 @@
-<?php 
+<?php
 class User_utils {
 	
+	public $CI;
+	
+	public function __construct() {
+		if (!isset($this->CI))
+        {
+            $this->CI =& get_instance();
+        }
+        $this->CI->load->library('session');
+	}
+
 	public function get_current_user_id() {
-		$user_id = $this->session->userdata('user_id');
-		if(!$user_id) {
+		$user_id = $this->CI->session->userdata('id');
+		if (!$user_id) {
 			return FALSE;
 		}
 		return $user_id;
 	}
+
 }
