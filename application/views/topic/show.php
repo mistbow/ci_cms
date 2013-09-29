6d8ea0070167453a9a969c2a25ccde50b4adae96
unknown <liu@.(none)>
2013-09-29 21:45:04 +0800

    add edit icon

--- application/views/topic/show.php
+++ application/views/topic/show.php
@@ -13,7 +13,22 @@
 	<div style="margin-left:20px;">
 		<?php echo $topic->body; ?>
 	</div>
-</br>
+<div class="tools pull-right">
+        
+    <a class="likeable" data-count="0" data-id="14457" data-state="" data-type="Topic" href="#" onclick="return App.likeable(this);" rel="twipsy" data-original-title="喜欢">
+    	<span class="glyphicon glyphicon-heart-empty"></span>
+    	<span>喜欢</span>
+    </a>
+    <a class="icon small_bookmark" data-id="14457" href="#" onclick="return Topics.favorite(this);" rel="twipsy" data-original-title="收藏"></a>
+    <a class="icon small_edit" href="/topics/14457/edit" title="修改本帖">
+		<span class="glyphicon glyphicon-check"></span>
+    </a>
+    <a class="icon small_delete" data-confirm="确定要删除么？" data-method="delete" href="/topics/14457" rel="nofollow" title="删除本帖">
+    	<span class="glyphicon glyphicon-trash"></span>
+    </a>
+</div>
+
+<div class="post_hidden"></div>
 
 </div>
 
