f4e177cbe2bb336cd9c7d1d94ea90eff446ea5ff
unknown <Administrator@.(none)>
2013-10-01 15:34:14 +0800

    get super class

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -2,8 +2,9 @@
 
 if (!function_exists('is_mine')) {
 	function is_mine($user_id) {
-		if($this->session->userdata('loggedin') 
-			&& $session->userdata('id') == $user_id) {
+		$CI =& get_instance();
+		if($CI->session->userdata('loggedin') 
+			&& $CI->session->userdata('id') == $user_id) {
 			return TRUE;
 		} else {
 			return FALSE;
