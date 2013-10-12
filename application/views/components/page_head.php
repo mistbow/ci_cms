13d81e0aadbb0dcb99677987d5acb5706c95f56d
unknown <liu@.(none)>
2013-10-12 22:01:21 +0800

    change ui

--- application/views/components/page_head.php
+++ application/views/components/page_head.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
-		<meta charset="utf-8">
-		<title><?php echo $site_name; ?></title>
-		<link href="<?php echo base_url('css/style.css'); ?>" rel="stylesheet">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	</head>
+  <head>
+    <title>6bey</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <title><?php echo $site_name; ?></title>
+	<link href="<?php echo base_url('css/style.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
+  </head>
--- application/views/components/page_nav.php
+++ application/views/components/page_nav.php
@@ -1,57 +1,53 @@
-<nav class="navbar navbar-inverse" role="navigation">
-	<div class="container">
-	  <!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="<?php echo base_url(); ?>"><?php echo $site_name; ?></a>
-		</div> <!-- navbar-header -->
-
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<ul class="nav navbar-nav">
-				<li class="active"><?php echo anchor('topic/index', '学习社区'); ?></li>
-				<li class="active"><?php echo anchor('exercise/index', '习题精讲'); ?></li>
+<div class="navbar navbar-inverse">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+          	</a>
+          	<a class="brand" href="<?php echo base_url(); ?>">
+          		<?php echo $site_name; ?>
+          	</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+            	<li class="active"><?php echo anchor('www.6bey.com', '首页'); ?></li>
+	        	<li class="active"><?php echo anchor('topic/index', '讨论区'); ?></li>
 				<li class="active"><?php echo anchor('ask/index', '问答社区'); ?></li>
-				<li class="active"><?php echo anchor('problem/index', '大题库'); ?></li>
-				
-			</ul>
-			<?php if(is_array($this->session->userdata) 
+				<li class="active"><?php echo anchor('exercise/index', '师兄笔记'); ?></li>
+				<li class="active"><?php echo anchor('problem/index', '习题库'); ?></li>
+            </ul>
+            <?php if(is_array($this->session->userdata) 
 				&& isset($this->session->userdata['loggedin']) 
 				&& $this->session->userdata['loggedin']) :?>
-			<ul class="nav navbar-nav navbar-right">
-				<li class="dropdown">
-					<a class="dropdown-toggle" href="<?php echo base_url(); ?>/active/message">
-      					<span class="glyphicon glyphicon-envelope"></span> 站内信
-      					<span class="badge badge-info msg_num"></span>
-      				</a>
-      			</li>
-
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<?php echo $this->session->userdata['username']; ?> 
-						<b class="caret"></b>
-					</a>
-					<ul class="dropdown-menu">
-					  <li><a href="/user/show">个人资料</a></li>
-					  <li><a href="#">个人空间</a></li>
-					  <li><a href="#">我的习题集</a></li>
-					  <li class="divider">&nbsp;</li>
-					  <li><?php echo anchor('user/logout', '退出'); ?></li>
-					</ul>
-				</li>
-			</ul>
-			<?php else : ?>
-			<ul class="nav navbar-nav navbar-right">
+            <ul class="nav pull-right">
+            	<li class="dropdown">
+                    <a class="dropdown-toggle" href="http://www.6bey.com//active/message">
+                          <span class="glyphicon glyphicon-envelope"></span> 站内信
+                          <span class="badge badge-info msg_num"></span>
+                        </a>
+                  </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">你好,
+                   <?php echo $this -> session -> userdata['username']; ?> <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="/user/show">个人资料</a></li>
+                  <li><a href="/user/avatar/">修改头像</a></li>
+                  <li><a href="/user/emailsetting/">邮件通知选项</a></li>
+                  <li><a href="/user/weibo/">绑定新浪微博</a></li>
+                  <li class="divider"></li>
+                  <li><a href="/user/logout/">退出</a></li>
+                 </ul>
+              </li>
+            </ul>
+            <?php else : ?>
+			<ul class="nav pull-right">
 				<li><?php echo anchor('user/register', '注册'); ?></li>
 				<li><?php echo anchor('qq/login', '登录'); ?></li>
 			</ul>
 			<?php endif; ?>
-		</div> <!-- end collapse navbar-collapse navbar-ex1-collapse -->
-
-	</div> <!-- end container -->
-</nav>
+                      
+          </div>
+        </div>
+      </div>
+</div><!-- end navbar -->
