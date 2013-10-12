bae8eb2c7690eac66e24d4d384739f8653cce2d3
unknown <liu@.(none)>
2013-10-12 22:53:30 +0800

    change sidebar

--- application/views/components/page_sidebar.php
+++ application/views/components/page_sidebar.php
@@ -1,27 +1,26 @@
 <div class="siderbar-unit" style="text-align:center;">
-  <?php echo anchor(base_url() . 'topic/newtopic'
+	<?php echo anchor(base_url() . 'topic/newtopic'
   		, '发布新帖', array('class'=>"btn btn-success")) ?>
-</div>
-			    
-<div class="panel panel-default">
-	<div class="panel-heading">微信二维码</div>
-	<div class="panel-body" >
-		<img  class=pull-center src="<?php echo base_url() . '/img/8cm.jpg'; ?>" height="100%" width="100%"/>
+</div> <!-- end siderbar-unit -->
+
+<div class="siderbar-unit" style="text-align:center;">
+	<h2>微信二维码</h2>
+	<div class="content" style="text-align:center">
+		<img class="pull-center" src="<?php echo base_url() . '/img/8cm.jpg'; ?>" height="100%" width="100%"/>
 		<h4 class="text-center">微信号：calvin6bey</h4>
 	</div>
-</div>
-
-<div class="panel panel-default">
-	<div class="panel-heading">社区最新动态</div>
-	<div class="panel-body">
-		<div>
-			<a href="/topic/4fa69d90bac222c3780576d8" target="_blank">如何获得6bey邀请码?</a>
-		</div>
-		<div>
-			<a href="/topic/4f4d9f007f999acb7c00af57" target="_blank">如何获得6bey官方T-Shirt?</a> 
-	  	</div>
-		<div>
-			<a href="/topic/4f17948d9f40a8335200518e" target="_blank">如何加入6bey社区?</a>
-		</div>
+</div>	
+		    
+<div class="siderbar-unit" style="text-align:center;">
+	<h2>社区最新动态</h2>
+	<div class="content" style="text-align:center">
+	<div>
+    	<a href="/topic/1" target="_blank">如何获得6bey邀请码?</a>
 	</div>
+	<div>
+    	<a href="/topic/2" target="_blank">如何获得6bey官方T-Shirt?</a> 
+    </div>
+  	<div>
+   	 <a href="/topic/3" target="_blank">如何加入6bey社区?</a>
+  	</div>
 </div>
