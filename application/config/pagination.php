cccc17ff282119bb7aaa272e4d8b8f174ccecfdc
unknown <liu@.(none)>
2013-10-12 23:32:39 +0800

    remove curren

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -1,8 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $config['total_rows'] = 200;
-$config['per_page'] = 5;
+$config['per_page'] = 10;
 $config['use_page_numbers'] = TRUE;
 $config['full_tag_open'] = '<ul class="pagination">';
 $config['full_tag_close'] = '</ul>';
 $config['cur_tag_open'] = '<li class="active"><a href="#">';
-$config['cur_tag_close'] = '<span class="sr-only">(current)</span></a></li>';
