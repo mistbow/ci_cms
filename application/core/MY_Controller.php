beb6b029fb9ccb0588622012d3f7be762e299c08
unknown <liu@.(none)>
2013-11-03 17:57:13 +0800

    change MY_Model code

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,202 +1,125 @@
 <?php
-/**
- * A base controller for CodeIgniter with view autoloading, layout support,
- * model loading, helper loading, asides/partials and per-controller 404
- *
- * @link http://github.com/jamierumbelow/codeigniter-base-controller
- * @copyright Copyright (c) 2012, Jamie Rumbelow <http://jamierumbelow.net>
- */
 
-class MY_Controller extends CI_Controller
-{
+class MY_Controller extends CI_Controller {
 
-    /* --------------------------------------------------------------
-     * VARIABLES
-     * ------------------------------------------------------------ */
+	protected $view = '';
 
-    /**
-     * The current request's view. Automatically guessed
-     * from the name of the controller and action
-     */
-    protected $view = '';
+	protected $data = array();
 
-    /**
-     * An array of variables to be passed through to the
-     * view, layout and any asides
-     */
-    protected $data = array();
+	protected $layout;
 
-    /**
-     * The name of the layout to wrap around the view.
-     */
-    protected $layout;
+	protected $asides = array();
 
-    /**
-     * An arbitrary list of asides/partials to be loaded into
-     * the layout. The key is the declared name, the value the file
-     */
-    protected $asides = array();
+	protected $models = array();
 
-    /**
-     * A list of models to be autoloaded
-     */
-    protected $models = array();
+	protected $model_string = '%_model';
 
-    /**
-     * A formatting string for the model autoloading feature.
-     * The percent symbol (%) will be replaced with the model name.
-     */
-    protected $model_string = '%_model';
+	protected $helpers = array();
 
-    /**
-     * A list of helpers to be autoloaded
-     */
-    protected $helpers = array();
+	public function __construct() {
+		parent::__construct();
+		$this -> data['errors'] = array();
+		$this -> data['site_name'] = config_item('site_name');
+		$this -> _load_models();
+		$this -> _load_helpers();
+	}
 
-    /* --------------------------------------------------------------
-     * GENERIC METHODS
-     * ------------------------------------------------------------ */
+	public function _remap($method) {
+		if (method_exists($this, $method)) {
+			call_user_func_array(array($this, $method), array_slice($this -> uri -> rsegments, 2));
+		} else {
+			if (method_exists($this, '_404')) {
+				call_user_func_array(array($this, '_404'), array($method));
+			} else {
+				show_404(strtolower(get_class($this)) . '/' . $method);
+			}
+		}
 
-    /**
-     * Initialise the controller, tie into the CodeIgniter superobject
-     * and try to autoload the models and helpers
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->data['errors'] = array();
-        $this->data['site_name'] = config_item('site_name');
-        $this->_load_models();
-        $this->_load_helpers();
-    }
+		$this -> _load_view();
+	}
 
-    /* --------------------------------------------------------------
-     * VIEW RENDERING
-     * ------------------------------------------------------------ */
+	/**
+	 * Automatically load the view, allowing the developer to override if
+	 * he or she wishes, otherwise being conventional.
+	 */
+	protected function _load_view() {
+		// If $this->view == FALSE, we don't want to load anything
+		if ($this -> view !== FALSE) {
+			// If $this->view isn't empty, load it. If it isn't, try and guess based on the controller and action name
+			$view = (!empty($this -> view)) ? $this -> view : $this -> router -> directory . $this -> router -> class . '/' . $this -> router -> method;
 
-    /**
-     * Override CodeIgniter's despatch mechanism and route the request
-     * through to the appropriate action. Support custom 404 methods and
-     * autoload the view into the layout.
-     */
-    public function _remap($method)
-    {
-        if (method_exists($this, $method))
-        {
-            call_user_func_array(array($this, $method), array_slice($this->uri->rsegments, 2));
-        }
-        else
-        {
-            if (method_exists($this, '_404'))
-            {
-                call_user_func_array(array($this, '_404'), array($method));
-            }
-            else
-            {
-                show_404(strtolower(get_class($this)).'/'.$method);
-            }
-        }
+			// Load the view into $yield
+			$data['yield'] = $this -> load -> view($view, $this -> data, TRUE);
 
-        $this->_load_view();
-    }
+			// Do we have any asides? Load them.
+			if (!empty($this -> asides)) {
+				foreach ($this->asides as $name => $file) {
+					$data['yield_' . $name] = $this -> load -> view($file, $this -> data, TRUE);
+				}
+			}
 
-    /**
-     * Automatically load the view, allowing the developer to override if
-     * he or she wishes, otherwise being conventional.
-     */
-    protected function _load_view()
-    {
-        // If $this->view == FALSE, we don't want to load anything
-        if ($this->view !== FALSE)
-        {
-            // If $this->view isn't empty, load it. If it isn't, try and guess based on the controller and action name
-            $view = (!empty($this->view)) ? $this->view : $this->router->directory . $this->router->class . '/' . $this->router->method;
+			// Load in our existing data with the asides and view
+			$data = array_merge($this -> data, $data);
+			$layout = FALSE;
 
-            // Load the view into $yield
-            $data['yield'] = $this->load->view($view, $this->data, TRUE);
+			// If we didn't specify the layout, try to guess it
+			if (!isset($this -> layout)) {
+				if (file_exists(APPPATH . 'views/layouts/' . $this -> router -> class . '.php')) {
+					$layout = 'layouts/' . $this -> router -> class;
+				} else {
+					$layout = 'layouts/application';
+				}
+			}
 
-            // Do we have any asides? Load them.
-            if (!empty($this->asides))
-            {
-                foreach ($this->asides as $name => $file)
-                {
-                    $data['yield_'.$name] = $this->load->view($file, $this->data, TRUE);
-                }
-            }
+			// If we did, use it
+			else if ($this -> layout !== FALSE) {
+				$layout = $this -> layout;
+			}
 
-            // Load in our existing data with the asides and view
-            $data = array_merge($this->data, $data);
-            $layout = FALSE;
+			// If $layout is FALSE, we're not interested in loading a layout, so output the view directly
+			if ($layout == FALSE) {
+				$this -> output -> set_output($data['yield']);
+			}
 
-            // If we didn't specify the layout, try to guess it
-            if (!isset($this->layout))
-            {
-                if (file_exists(APPPATH . 'views/layouts/' . $this->router->class . '.php'))
-                {
-                    $layout = 'layouts/' . $this->router->class;
-                }
-                else
-                {
-                    $layout = 'layouts/application';
-                }
-            }
+			// Otherwise? Load away :)
+			else {
+				$this -> load -> view($layout, $data);
+			}
+		}
+	}
 
-            // If we did, use it
-            else if ($this->layout !== FALSE)
-            {
-                $layout = $this->layout;
-            }
+	/* --------------------------------------------------------------
+	 * MODEL LOADING
+	 * ------------------------------------------------------------ */
 
-            // If $layout is FALSE, we're not interested in loading a layout, so output the view directly
-            if ($layout == FALSE)
-            {
-                $this->output->set_output($data['yield']);
-            }
+	/**
+	 * Load models based on the $this->models array
+	 */
+	private function _load_models() {
+		foreach ($this->models as $model) {
+			$this -> load -> model($this -> _model_name($model), $model);
+		}
+	}
 
-            // Otherwise? Load away :)
-            else
-            {
-                $this->load->view($layout, $data);
-            }
-        }
-    }
+	/**
+	 * Returns the loadable model name based on
+	 * the model formatting string
+	 */
+	protected function _model_name($model) {
+		return str_replace('%', $model, $this -> model_string);
+	}
 
-    /* --------------------------------------------------------------
-     * MODEL LOADING
-     * ------------------------------------------------------------ */
+	/* --------------------------------------------------------------
+	 * HELPER LOADING
+	 * ------------------------------------------------------------ */
 
-    /**
-     * Load models based on the $this->models array
-     */
-    private function _load_models()
-    {
-        foreach ($this->models as $model)
-        {
-            $this->load->model($this->_model_name($model), $model);
-        }
-    }
+	/**
+	 * Load helpers based on the $this->helpers array
+	 */
+	private function _load_helpers() {
+		foreach ($this->helpers as $helper) {
+			$this -> load -> helper($helper);
+		}
+	}
 
-    /**
-     * Returns the loadable model name based on
-     * the model formatting string
-     */
-    protected function _model_name($model)
-    {
-        return str_replace('%', $model, $this->model_string);
-    }
-
-    /* --------------------------------------------------------------
-     * HELPER LOADING
-     * ------------------------------------------------------------ */
-
-    /**
-     * Load helpers based on the $this->helpers array
-     */
-    private function _load_helpers()
-    {
-        foreach ($this->helpers as $helper)
-        {
-            $this->load->helper($helper);
-        }
-    }
 }
