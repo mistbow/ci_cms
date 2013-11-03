68b2131efe90b293faecddd9bfab65200e29d300
mistbow <guofuchunlh@gmail.com>
2013-11-03 21:54:39 +0800

    change layout content

--- application/views/components/register_subview.php
+++ application/views/components/register_subview.php
@@ -1,30 +1,30 @@
 <div class="modal-header">
-	<h3>Log in</h3>
-	<p>Please log in using your credentials</p>
+	<h3>用户注册</h3>
+	<p>欢迎注册6bey学堂,Happy everyday!</p>
 </div>
 <div class="modal-body">
 <?php echo validation_errors(); ?>
 <?php echo form_open();?>
 <table class="table">
 	<tr>
-		<td>Email</td>
+		<td>邮箱</td>
 		<td><?php echo form_input('email'); ?></td>
 	</tr>
 	<tr>
-		<td>Username</td>
+		<td>昵称</td>
 		<td><?php echo form_input('username'); ?></td>
 	</tr>
 	<tr>
-		<td>Password</td>
+		<td>密码</td>
 		<td><?php echo form_password('password'); ?></td>
 	</tr>
 	<tr>
-		<td>confirm password</td>
+		<td>密码确认</td>
 		<td><?php echo form_password('password_confirmation'); ?></td>
 	</tr>
 	<tr>
 		<td></td>
-		<td><?php echo form_submit('submit', 'Registe', 'class="btn btn-primary"'); ?></td>
+		<td><span style="padding-right:20px">填写完信息请点击提交</span><?php echo form_submit('submit', '提交', 'class="btn btn-primary"'); ?></td>
 	</tr>
 </table>
 <?php echo form_close();?>
--- application/views/layouts/user.php
+++ application/views/layouts/user.php
@@ -2,9 +2,6 @@
 <body>
 <?php $this->load->view('components/page_nav'); ?>
 <div class="container">
-	<div class="alert alert-info">
-    	<a href="/">6bey又一次蛋疼的改版界面了</a>
-	</div> 
     <div class="row">
     	<?php echo $yield; ?>
 	</div><!-- end row-->
